b3271215425d30e32107d54273e5c096c2dc03cc
LeeJay124 <57182164+LeeJay124@users.noreply.github.com>
2019-11-02 15:47:52 -0400

    Initial Commit

---
+++ VSSquad/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  VSSquad
+//
+//  Created by Lisa Brown on 10/16/19.
+//  Copyright © 2019 Lisa Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var Username: UILabel!
+    @IBOutlet weak var UsernameField: UITextField!
+    
+    @IBOutlet weak var Password: UILabel!
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    //MARK: Action
+    
+   @IBAction func Login(_ sender: UIButton) {
+   }
+//    @IBAction func GoToUserDisplay(_ sender: UIButton) {
+//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
